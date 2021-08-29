52be9813ba5c3161dba327d7dfade05e403b5142
Samuel89Su <sushoucai1989@hotmail.com>
2021-08-29 17:34:46 +0800

    add viewer as embedded sources

--- NLog.Contrib.Targets.WebSocket.Web.AspNetCore/WebSocketConnectionMiddleware.cs
+++ NLog.Contrib.Targets.WebSocket.Web.AspNetCore/WebSocketConnectionMiddleware.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -84,26 +86,51 @@ namespace NLog.Contrib.Targets.WebSocket.Web.AspNetCore
         {
             if (requestPath.EndsWith(Constants.PATH_VIEWER))
             {
-                requestPath += Constants.VIEWER_INDEX;
+                context.Response.StatusCode = (int)HttpStatusCode.MovedPermanently;
+                context.Response.Headers.TryAdd("Location", requestPath + Constants.VIEWER_INDEX);
+                await context.Response.WriteAsync(string.Empty, Encoding.UTF8);
+                
+                return;
             }
-            var filePath = Path.Combine(new List<string> { BaseDirectory }.Concat(requestPath.Replace(Constants.PATH_VIEWER, Constants.VIEWER_DIR).Trim('/', ' ').Replace("//", "/").Split("/")).ToArray());
 
-            var contentType = filePath.EndsWith(Constants.EXTENSION_HTML)
+            var resourceSuffix = requestPath.Split('/').Last();
+
+            var contentType = requestPath.EndsWith(Constants.EXTENSION_HTML)
                                ? Constants.MEDIATYPE_HTML
-                               : filePath.EndsWith(Constants.EXTENSION_JS)
+                               : requestPath.EndsWith(Constants.EXTENSION_JS)
                                ? Constants.MEDIATYPE_JS
-                               : filePath.EndsWith(Constants.EXTENSION_CSS)
+                               : requestPath.EndsWith(Constants.EXTENSION_CSS)
                                ? Constants.MEDIATYPE_CSS
                                : throw new FileNotFoundException();
-            if (File.Exists(filePath))
+
+            var resourseId = "NLog.Contrib.Targets.WebSocket.Web.AspNetCore.WebSocketLoggerViewer."
+                             + resourceSuffix;
+
+            try
             {
-                context.Response.ContentType = contentType;
-                await context.Response.WriteAsync(await File.ReadAllTextAsync(filePath), Encoding.UTF8);
+
+                using (var stream = this.GetType().Assembly.GetManifestResourceStream(resourseId))
+                using (var sr = new StreamReader(stream))
+                {
+                    var content = await sr.ReadToEndAsync();
+                    context.Response.ContentType = contentType;
+                    await context.Response.WriteAsync(content, Encoding.UTF8);
+                }
             }
-            else
+            catch (Exception ex)
             {
                 throw new FileNotFoundException();
             }
+
+                //if (File.Exists(filePath))
+                //{
+                //    context.Response.ContentType = contentType;
+                //    await context.Response.WriteAsync(await File.ReadAllTextAsync(filePath), Encoding.UTF8);
+                //}
+                //else
+                //{
+                //    throw new FileNotFoundException();
+                //}
         }
 
         private static Dictionary<string, string> FindMatches(string requestPath, IEnumerable<WebSocketRouteAttribute> routeAttributes)
